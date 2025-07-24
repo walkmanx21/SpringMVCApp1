497404d295eebc74033561773c71567dec78de9f
walkmanx21 <walkmanx21@gmail.com>
2025-07-24 20:15:50 +0300

    Окончание дня 24.07.2025

---
+++ src/main/java/org/walkmanx21/config/MySpringMvсDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package org.walkmanx21.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvсDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
---
+++ src/main/java/org/walkmanx21/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.walkmanx21.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
